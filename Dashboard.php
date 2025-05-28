a195587951aada6db934b25e0decf1a85a56b364
daffanoviar <daffanov10@gmail.com>
2025-05-28 17:38:46 +0700

    tambahan untuk uts

--- Dashboard.php
+++ Dashboard.php
@@ -3,6 +3,14 @@ session_start();
 if (!isset($_SESSION['username'])) {
     // User is already logged in, redirect to welcome page  
     header("Location: login.php");
+    exit();
+}
+
+//cek apakah session counter sudah diset
+if (!isset($_SESSION['counter'])){
+    $_SESSION['counter']=1;
+}else{
+    $_SESSION['counter']+=1;
 }
 ?>
 <html>
@@ -18,6 +26,6 @@ if (!isset($_SESSION['username'])) {
         </style>
     </head>
     <body>
-        <h1><?php echo "Selamat datang " . $_SESSION['username']; ?></h1>
+        <h1><?php echo "Selamat datang" . $_SESSION['username']; ?></h1>
     </body>
 </html>
